283e05b9de30a4324d1794540ea575f377059be2
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-03-06 18:00:45 +0000

    no message

---
+++ ParcelEnterpriseApplication/ParcelEnterpriseApplication-ejb/src/java/uk/co/giovannilenguito/entities/facade/GatewayFacade.java
@@ -0,0 +1,12 @@
+package uk.co.giovannilenguito.entities.facade;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Giovanni Lenguito <www.giovannilenguito.co.uk>
+ */
+@Stateless
+public class GatewayFacade implements GatewayFacadeLocal {
+    
+}
---
+++ ParcelEnterpriseApplication/ParcelEnterpriseApplication-ejb/src/java/uk/co/giovannilenguito/entities/facade/GatewayFacadeLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.co.giovannilenguito.entities.facade;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Giovanni Lenguito <www.giovannilenguito.co.uk>
+ */
+@Local
+public interface GatewayFacadeLocal {
+    
+}
